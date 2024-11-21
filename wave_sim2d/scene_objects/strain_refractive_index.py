680141c55d5da91df825c09ea0d0bfa6a1d319fa
0x23 <diffraction.lim@gmail.com>
2024-11-21 09:45:25 +0100

    tests

--- wave_sim2d/scene_objects/strain_refractive_index.py
+++ wave_sim2d/scene_objects/strain_refractive_index.py
@@ -17,15 +17,17 @@ class StrainRefractiveIndex(SceneObject):
         self.coupling_constant = coupling_constant
         self.refractive_index_offset = refractive_index_offset
 
-        self.strain_kernel = cp.array([[0.05, 0.2, 0.05],
-                                       [0.2, -1.0, 0.2],
-                                       [0.05, 0.2, 0.05]])
+        self.du_dx_kernel = cp.array([[-1, 0.0, 1]])
+        self.du_dy_kernel = cp.array([[-1], [0.0], [1]])
 
         self.strain_field = None
 
     def render(self, field: cp.ndarray, wave_speed_field: cp.ndarray, dampening_field: cp.ndarray):
         # compute strain
-        self.strain_field = cupyx.scipy.signal.convolve2d(field, self.strain_kernel, mode='same', boundary='fill')
+        du_dx = cupyx.scipy.signal.convolve2d(field, self.du_dx_kernel, mode='same', boundary='fill')
+        du_dy = cupyx.scipy.signal.convolve2d(field, self.du_dy_kernel, mode='same', boundary='fill')
+
+        self.strain_field = cp.sqrt(du_dx**2 + du_dy**2)
 
         # compute refractive index from strain
         refractive_index_field = self.refractive_index_offset + self.strain_field*self.coupling_constant
